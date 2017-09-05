6f682708007888202f7d27c5167f69c5d3527eb5
yang24201 <yang24201@126.com>
2017-09-06 00:27:39 +0800

    and Switch and NoMatch

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,16 +1,18 @@
 // We only need to import the modules necessary for initial render
 import React from 'react'
-import {Route} from 'react-router-dom'
+import {Route, Switch} from 'react-router-dom'
 import Login from './Login'
 import Home from './Home'
 import About from '../components/About'
 import NoMatch from './NoMatch'
 
 const rootRoutes = [
-  <Route key="home" exact path="/" component={Home}/>,
-  <Route key="login" exact path="/login" component={Login}/>,
-  <Route key="about" exact path="/about" component={About}/>,
-  // <Route key="noMatch" component={NoMatch}/>,
+  <Switch>
+    <Route key="home" exact path="/" component={Home}/>
+    <Route key="login" path="/login" component={Login}/>
+    <Route key="about" path="/about" component={About}/>
+    <Route key="noMatch" component={NoMatch}/>
+  </Switch>,
 ]
 
 export default rootRoutes
