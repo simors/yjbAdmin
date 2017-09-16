14ed4967056f3350ae145d31aff30fdca4c702b7
luorunbing <luorunbing@lvyii.com>
2017-09-16 19:13:46 +0800

    initial sysuser
    Signed-off-by: luorunbing <luorunbing@lvyii.com>

--- src/routes/Home/ContentRouter.js
+++ src/routes/Home/ContentRouter.js
@@ -5,12 +5,14 @@ import React from 'react'
 import {Route, Switch} from 'react-router-dom'
 import Dashboard from '../../components/Dashboard'
 import SysConfig from '../../components/SysConfig'
+import SysUser from '../sysuser/SysUser'
 
 const ContentRouter = ({match}) => {
   return (
     <Switch>
       <Route exact path={match.url} component={Dashboard}/>
       <Route exact path="/cabinet/list" component={SysConfig} />
+      <Route exact path="/system/user" component={SysUser} />
     </Switch>
   )
 }
