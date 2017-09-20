7076f0af79f2846541a97e077849ad8f81248ceb
luorunbing <luorunbing@lvyii.com>
2017-09-21 00:13:21 +0800

    restructure redux for sysuser
    Signed-off-by: luorunbing <luorunbing@lvyii.com>

--- src/route/sysuser/redux.js
+++ src/route/sysuser/redux.js
@@ -1,12 +1,176 @@
-import {createAction} from 'redux-actions';
+import {createAction} from './util';
 import {call, put, takeLatest} from 'redux-saga/effects';
-import {Record, List} from 'immutable';
+import {Record, List, Map} from 'immutable';
 import * as api from './api';
+
+// --- Action
+
+const SAGA_USER_LIST = "SAGA@sysuser/USER/LIST";
+const SAGA_USER_SHOW = "SAGA@sysuser/USER/SHOW";
+const SAGA_USER_CREATE = "SAGA@sysuser/USER/CREATE";
+const SAGA_USER_EDIT = "SAGA@sysuser/USER/EDIT";
+const SAGA_USER_DELETE = "SAGA@sysuser/USER/DELETE";
+const SAGA_USER_SAVE = "SAGA@sysuser/USER/SAVE";
+
+export const action = {
+  userList: createAction(SAGA_USER_LIST),
+  userShow: createAction(SAGA_USER_SHOW),
+  userCreate: createAction(SAGA_USER_CREATE),
+  userEdit: createAction(SAGA_USER_EDIT),
+  userDelete: createAction(SAGA_USER_DELETE),
+  userSave: createAction(SAGA_USER_SAVE),
+};
+
+const REDUCER_USER_LIST = "REDUCER@sysuser/USER/LIST";
+const REDUCER_USER_SHOW = "REDUCER@sysuser/USER/SHOW";
+const REDUCER_USER_CREATE = "REDUCER@sysuser/USER/CREATE";
+const REDUCER_USER_EDIT = "REDUCER@sysuser/USER/EDIT";
+const REDUCER_USER_DELETE = "REDUCER@sysuser/USER/DELETE";
+const REDUCER_USER_SAVE = "REDUCER@sysuser/USER/SAVE";
+
+const reducerAction = {
+  userList: createAction(REDUCER_USER_LIST),
+  userShow: createAction(REDUCER_USER_SHOW),
+  userCreate: createAction(REDUCER_USER_CREATE),
+  userEdit: createAction(REDUCER_USER_EDIT),
+  userDelete: createAction(REDUCER_USER_DELETE),
+  userSave: createAction(REDUCER_USER_SAVE),
+};
+
+// --- Saga
+
+function* saga_userList(action) {
+  const payload = action.payload;
+
+  const res = yield call(api.userList, payload);
+  if (res.success) {
+    // transform from http json response to an array of immutable.js Record
+    let users = [];
+    res.users.forEach((i) => {
+      users.push(User.fromJsonApi(i));
+    });
+
+    yield put(reducerAction.userList({users}));
+
+    if (payload.onSuccess) {
+      payload.onSuccess();
+    }
+  } else {
+    if (payload.onFailure) {
+      payload.onFailure();
+    }
+  }
+}
+
+function* saga_userShow(action) {
+  const payload = action.payload;
+
+  const res = yield call(api.userShow, payload);
+  if (res.success) {
+    const user = User.fromJsonApi(res.user);
+
+    yield put(reducerAction.userShow({user}));
+
+    if (payload.onSuccess) {
+      payload.onSuccess();
+    }
+  } else {
+    if (payload.onFailure) {
+      payload.onFailure();
+    }
+  }
+}
+
+function* saga_userCreate(action) {
+  const payload = action.payload;
+
+  const res = yield call(api.userCreate, payload);
+  if (res.success) {
+    const user = User.fromJsonApi(res.user);
+
+    yield put(reducerAction.userCreate({user}));
+
+    if (payload.onSuccess) {
+      payload.onSuccess();
+    }
+  } else {
+    if (payload.onFailure) {
+      payload.onFailure();
+    }
+  }
+}
+
+function* saga_userEdit(action) {
+  const payload = action.payload;
+
+  const res = yield call(api.userEdit, payload);
+  if (res.success) {
+    const user = User.fromJsonApi(res.user);
+
+    yield put(reducerAction.userEdit({user}));
+
+    if (payload.onSuccess) {
+      payload.onSuccess();
+    }
+  } else {
+    if (payload.onFailure) {
+      payload.onFailure();
+    }
+  }
+}
+
+function* saga_userDelete(action) {
+  const payload = action.payload;
+
+  const res = yield call(api.userDelete, payload);
+  if (res.success) {
+    const user = User.fromJsonApi(res.user);
+
+    yield put(reducerAction.userDelete({user}));
+
+    if (payload.onSuccess) {
+      payload.onSuccess();
+    }
+  } else {
+    if (payload.onFailure) {
+      payload.onFailure();
+    }
+  }
+}
+
+function* saga_userSave(action) {
+  const payload = action.payload;
+
+  const res = yield call(api.userSave, payload);
+  if (res.success) {
+    const user = User.fromJsonApi(res.user);
+
+    yield put(reducerAction.userSave({user}));
+
+    if (payload.onSuccess) {
+      payload.onSuccess();
+    }
+  } else {
+    if (payload.onFailure) {
+      payload.onFailure();
+    }
+  }
+}
+
+export const saga = [
+  takeLatest(SAGA_USER_LIST, saga_userList),
+  takeLatest(SAGA_USER_SHOW, saga_userShow),
+  takeLatest(SAGA_USER_CREATE, saga_userCreate),
+  takeLatest(SAGA_USER_EDIT, saga_userEdit),
+  takeLatest(SAGA_USER_DELETE, saga_userDelete),
+  takeLatest(SAGA_USER_SAVE, saga_userSave),
+];
 
 // --- State
 
 const UserState = Record({
-  users: List(),
+  userIds: List(),
+  users: Map(),
 }, "UserState");
 
 class User extends Record({
@@ -16,84 +180,66 @@ class User extends Record({
   note: null,
   roles: null
 }, "User") {
-  static fromJsonApi(jsonData) {
+  static fromJsonApi(json) {
     let user = new User();
-    return user.withMutations((i) => {
-      i.set('id', jsonData.id);
-      i.set('name', jsonData.name);
-      i.set('phoneNo', jsonData.phoneNo);
-      i.set('note', jsonData.note);
-      i.set('roles', jsonData.roles);
+    return user.withMutations((m) => {
+      m.set('id', json.id);
+      m.set('name', json.name);
+      m.set('phoneNo', json.phoneNo);
+      m.set('note', json.note);
+      m.set('roles', json.roles);
     })
   }
 }
 
 // --- Selector
 
-function selectUser(appState) {
+function select_users(appState) {
   const state = appState.SYSUSER;
-  let users = [];
-  if (state.users.size > 0) {
-    state.users.forEach((i) => {
-      users.push(i);
-    })
-  }
-  return users;
+  return state.get('users').toArray();
+}
+
+function select_userIds(appState) {
+  const state = appState.SYSUSER;
+  return state.get('userIds').toArray();
 }
 
 export const selector = {
-  selectUser,
+  userIds: select_userIds,
+  users: select_users,
 };
-
-// --- Action
-
-const LIST_USER = "SysUser/LIST_USER";
-const LIST_USER_DONE = "SysUser/LIST_USER_DONE";
-const SAVE_ROLE = "SysUser/SAVE_ROLE";
-
-export const action = {
-  listUser: createAction(LIST_USER),
-  saveRole: createAction(SAVE_ROLE),
-};
-
-const listUserDone = createAction(LIST_USER_DONE);
-
-// --- Saga
-
-function* sagaListUser(action) {
-  const {r, users: rawUsers} = yield call(api.listUser, action.payload);
-  if (!r) {
-    // transform from http json response to an array of immutable.js Record
-    let users = [];
-    rawUsers.forEach((i) => {
-      users.push(User.fromJsonApi(i));
-    });
-
-    yield put(listUserDone({users}));
-  }
-}
-
-function* sagaSaveRole(action) {
-  console.log("[DEBUG] ---> sagaSaveRole, action: ", action);
-}
-
-export const saga = [
-  takeLatest(LIST_USER, sagaListUser),
-  takeLatest(SAVE_ROLE, sagaSaveRole),
-];
 
 // --- Reducer
 
-const reduceListUser = (state, action) => {
-  const users = action.payload.users;
-  state = state.set('users', new List(users));
-  return state;
-};
+function reduce_userList(state, action) {
+  const payload = action.payload;
+  const rawUsers = payload.users; // an array of immutable.js Record
+
+  let userIds = new List();
+  let users = new Map();
+
+  userIds = userIds.withMutations((m) => {
+    rawUsers.forEach((i) => {
+      m.push(i.id);
+    });
+  });
+
+  users = users.withMutations((m) => {
+    rawUsers.forEach((i) => {
+      m.set(i.id, i);
+    })
+  });
+
+  return state.withMutations((m) => {
+    m.set('userIds', userIds);
+    m.setIn(['users'], users);
+  });
+}
 
 export const reducer = (state=UserState(), action) => {
   switch (action.type) {
-    case LIST_USER_DONE:
-      return reduceListUser(state, action);
+    case REDUCER_USER_LIST:
+      return reduce_userList(state, action);
     default:
       return state;
   }
