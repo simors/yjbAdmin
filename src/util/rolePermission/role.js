73781a42a5801d22cf44a155003be7a8fc6819b2
yang24201 <yang24201@126.com>
2017-10-20 12:04:04 +0800

    user auth role in profit page

--- src/util/rolePermission/role.js
+++ src/util/rolePermission/role.js
@@ -6,6 +6,6 @@ export const ROLE_CODE = {
   PLATFORM_MANAGER:             100,        // 平台管理员
   STATION_MANAGER:              200,        // 服务点管理员
   STATION_INVESTOR:             300,        // 服务点投资人
-  STATION_DEPARTMENT:           400,        // 服务单位
+  STATION_PROVIDER:             400,        // 服务单位
   SYS_MANAGER:                  500,        // 系统管理员
 }
