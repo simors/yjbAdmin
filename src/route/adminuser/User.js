a6349fc7217ab1e8664a86c6b8ba9bdb7a6ee33f
luorunbing <luorunbing@lvyii.com>
2017-10-23 21:48:50 +0800

    user: reset filter on refresh for admin user
    Signed-off-by: luorunbing <luorunbing@lvyii.com>

--- src/route/adminuser/User.js
+++ src/route/adminuser/User.js
@@ -12,7 +12,15 @@ import UserEdit from './UserEdit';
 class User extends React.Component {
   constructor(props) {
     super(props);
+    this.state = {
+      isRefresh: false,
+    }
   }
+
+  refresh = () => {
+    this.setState({isRefresh: true})
+    setTimeout(() => this.setState({isRefresh: false}), 1000)
+  };
 
   render() {
     return (
@@ -20,8 +28,8 @@ class User extends React.Component {
         <Row type="flex" gutter={24}>
           <Col lg={{span: 18}}>
             <div style={{display: "flex", flexFlow: "column"}}>
-              <UserOp/>
-              <UserFilter/>
+              <UserOp onRefresh={this.refresh}/>
+              <UserFilter isRefresh={this.state.isRefresh}/>
               <UserList/>
             </div>
           </Col>
--- src/route/adminuser/UserOp.js
+++ src/route/adminuser/UserOp.js
@@ -14,6 +14,7 @@ class UserOp extends React.Component {
   };
 
   onRefresh = () => {
+    this.props.resetFilter({reset: true});
     this.props.listAdminUsers({limit: 100});
   };
 
