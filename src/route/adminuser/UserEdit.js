85f76856489d56ac3d0bb54e19958e0db9162d67
luorunbing <luorunbing@lvyii.com>
2017-10-31 00:02:50 +0800

    user: check station association before update roles
    Signed-off-by: luorunbing <luorunbing@lvyii.com>

--- src/route/adminuser/UserEdit.js
+++ src/route/adminuser/UserEdit.js
@@ -1,8 +1,10 @@
 import React from 'react';
 import {connect} from 'react-redux';
-import {Modal, Button, Form, Input, Select, Checkbox} from 'antd';
+import {Modal, Button, Form, Input, Select, Checkbox, message} from 'antd';
 import {action, selector} from './redux';
 import {action as authAction, selector as authSelector} from '../../util/auth/';
+import {stationAction} from '../station/';
+import {ROLE_CODE} from '../../util/rolePermission/';
 import style from './UserEdit.module.scss';
 
 class UserEdit extends React.Component {
@@ -26,6 +28,48 @@ class UserEdit extends React.Component {
     });
   };
 
+  validAdminHaveNoStation = (userId) => {
+    return new Promise((resolve, reject) => {
+      this.props.validAdminHaveNoStation({
+        userId,
+        success: () => {
+          resolve();
+        },
+        error: () => {
+          reject('取消用户服务点管理员角色，请先取消该用户与服务点的关联');
+        }
+      });
+    });
+  };
+
+  validInvestorHaveNoStation = (userId) => {
+    return new Promise((resolve, reject) => {
+      this.props.validInvestorHaveNoStation({
+        userId,
+        success: () => {
+          resolve();
+        },
+        error: () => {
+          reject('取消用户服务点投资人角色，请先取消该用户与服务点的关联');
+        }
+      });
+    });
+  };
+
+  validPartnerHaveNoStation = (userId) => {
+    return new Promise((resolve, reject) => {
+      this.props.validPartnerHaveNoStation({
+        userId,
+        success: () => {
+          resolve();
+        },
+        error: () => {
+          reject('取消用户服务单位角色，请先取消该用户与服务点的关联');
+        }
+      });
+    });
+  };
+
   onSubmit = (e) => {
     e.preventDefault();
     this.props.form.validateFieldsAndScroll((err, values) => {
@@ -40,25 +84,64 @@ class UserEdit extends React.Component {
         };
       });
 
-      this.props.updateUser({
-        params: {
-          id: this.props.user.id,
-          ...values,
+      const {id: userId} = this.props.user;
+      const {roles} = values;
+      const newRoles = new Set(roles);
+      const oldRoles = new Set(this.props.user.roles);
+      const uncheckAdmin = !newRoles.has(ROLE_CODE.STATION_MANAGER) && oldRoles.has(ROLE_CODE.STATION_MANAGER);
+      const uncheckInvestor = !newRoles.has(ROLE_CODE.STATION_INVESTOR) && oldRoles.has(ROLE_CODE.STATION_INVESTOR);
+      const uncheckPartner = !newRoles.has(ROLE_CODE.STATION_PROVIDER) && oldRoles.has(ROLE_CODE.STATION_PROVIDER);
+
+      Promise.resolve().then(
+        () => {
+          if (uncheckAdmin) {
+            return this.validAdminHaveNoStation(userId);
+          }
+        }
+      ).then(
+        () => {
+          if (uncheckInvestor) {
+            return this.validInvestorHaveNoStation(userId);
+          }
+        }
+      ).then(
+        () => {
+          if (uncheckPartner) {
+            return this.validPartnerHaveNoStation(userId);
+          }
+        }
+      ).then(
+        () => {
+          this.props.updateUser({
+            params: {
+              id: this.props.user.id,
+              ...values,
+            },
+            onSuccess: () => {
+              this.props.hideUserEditModal({});
+              this.props.form.resetFields();
+              this.props.listAdminUsers({limit: 100});
+            },
+            onComplete: () => {
+              this.setState((prevState, props) => {
+                return {
+                  ...prevState,
+                  loading: false,
+                };
+              });
+            },
+          });
         },
-        onSuccess: () => {
-          this.props.hideUserEditModal({});
-          this.props.form.resetFields();
-          this.props.listAdminUsers({limit: 100});
-        },
-        onComplete: () => {
+        (err) => {
+          message.error(err);
           this.setState((prevState, props) => {
             return {
               ...prevState,
               loading: false,
             };
           });
-        },
-      });
+        }
+      );
     });
   };
 
@@ -183,6 +266,7 @@ const mapStateToProps = (appState, ownProps) => {
 const mapDispatchToProps = {
   ...action,
   ...authAction,
+  ...stationAction,
 };
 
 export default connect(mapStateToProps, mapDispatchToProps)(Form.create()(UserEdit));
