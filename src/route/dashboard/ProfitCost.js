079715404c90ec1ee68d8711d303db94a87600a1
yang24201 <yang24201@126.com>
2017-11-03 18:54:01 +0800

    fetch platform profit data

--- src/route/dashboard/ProfitCost.js
+++ src/route/dashboard/ProfitCost.js
@@ -3,11 +3,16 @@
  */
 import React from 'react'
 import {connect} from 'react-redux'
-import {Row, Col, Card} from 'antd';
+import {Row, Col, Card} from 'antd'
+import {dashboardAction, dashboardSelector} from './redux'
 
 class ProfitCost extends React.Component {
   constructor(props) {
     super(props)
+  }
+
+  componentWillMount() {
+    this.props.requestPlatformProfitStat({})
   }
 
   renderTitleExtra(data, unit) {
@@ -39,39 +44,51 @@ class ProfitCost extends React.Component {
   }
 
   render() {
+    let {platformProfit} = this.props
+    if (!platformProfit
+      || !platformProfit.platformAccount
+      || !platformProfit.lastDayPlatformAccount
+      || !platformProfit.lastMonthPlatformAccount
+      || !platformProfit.lastYearPlatformAccount) {
+      return null
+    }
+    let platformAccount = platformProfit.platformAccount
+    let lastDayPlatformAccount = platformProfit.lastDayPlatformAccount
+    let lastMonthPlatformAccount = platformProfit.lastMonthPlatformAccount
+    let lastYearPlatformAccount = platformProfit.lastYearPlatformAccount
     return (
       <div>
         <Row gutter={16} style={{marginBottom: 20}}>
           <Col span={8}>
-            <Card title="营业额" extra={this.renderTitleExtra(0, '元')} style={{ width: '100%' }}>
-              {this.renderCardContent(0, 0, 0)}
+            <Card title="营业额" extra={this.renderTitleExtra(platformAccount.incoming || 0, '元')} style={{ width: '100%' }}>
+              {this.renderCardContent(lastDayPlatformAccount.incoming || 0, lastMonthPlatformAccount.incoming || 0, lastYearPlatformAccount.incoming || 0)}
             </Card>
           </Col>
           <Col span={8}>
-            <Card title="纯利润" extra={this.renderTitleExtra(0, '元')} style={{ width: '100%' }}>
-              {this.renderCardContent(0, 0, 0)}
+            <Card title="纯利润" extra={this.renderTitleExtra(platformAccount.profit || 0, '元')} style={{ width: '100%' }}>
+              {this.renderCardContent(lastDayPlatformAccount.profit || 0, lastMonthPlatformAccount.profit || 0, lastYearPlatformAccount.profit || 0)}
             </Card>
           </Col>
           <Col span={8}>
-            <Card title="电费成本" extra={this.renderTitleExtra(0, '元')} style={{ width: '100%' }}>
-              {this.renderCardContent(0, 0, 0)}
+            <Card title="电费成本" extra={this.renderTitleExtra(platformAccount.cost || 0, '元')} style={{ width: '100%' }}>
+              {this.renderCardContent(lastDayPlatformAccount.cost || 0, lastMonthPlatformAccount.cost || 0, lastYearPlatformAccount.cost || 0)}
             </Card>
           </Col>
         </Row>
         <Row gutter={16} style={{marginBottom: 20}}>
           <Col span={8}>
-            <Card title="平台分成" extra={this.renderTitleExtra(0, '元')} style={{ width: '100%' }}>
-              {this.renderCardContent(0, 0, 0)}
+            <Card title="平台分成" extra={this.renderTitleExtra(platformAccount.platformProfit || 0, '元')} style={{ width: '100%' }}>
+              {this.renderCardContent(lastDayPlatformAccount.platformProfit || 0, lastMonthPlatformAccount.platformProfit || 0, lastYearPlatformAccount.platformProfit || 0)}
             </Card>
           </Col>
           <Col span={8}>
-            <Card title="服务单位分成" extra={this.renderTitleExtra(0, '元')} style={{ width: '100%' }}>
-              {this.renderCardContent(0, 0, 0)}
+            <Card title="服务单位分成" extra={this.renderTitleExtra(platformAccount.partnerProfit || 0, '元')} style={{ width: '100%' }}>
+              {this.renderCardContent(lastDayPlatformAccount.partnerProfit || 0, lastMonthPlatformAccount.partnerProfit || 0, lastYearPlatformAccount.partnerProfit || 0)}
             </Card>
           </Col>
           <Col span={8}>
-            <Card title="投资人分成" extra={this.renderTitleExtra(0, '元')} style={{ width: '100%' }}>
-              {this.renderCardContent(0, 0, 0)}
+            <Card title="投资人分成" extra={this.renderTitleExtra(platformAccount.investorProfit || 0, '元')} style={{ width: '100%' }}>
+              {this.renderCardContent(lastDayPlatformAccount.investorProfit || 0, lastMonthPlatformAccount.investorProfit || 0, lastYearPlatformAccount.investorProfit || 0)}
             </Card>
           </Col>
         </Row>
@@ -81,12 +98,14 @@ class ProfitCost extends React.Component {
 }
 
 const mapStateToProps = (state, ownProps) => {
-
+  let platformProfit = dashboardSelector.selectPlatformProfitStat(state)
   return {
+    platformProfit,
   }
 }
 
 const mapDispatchToProps = {
+  ...dashboardAction,
 }
 
 export default connect(mapStateToProps, mapDispatchToProps)(ProfitCost)
