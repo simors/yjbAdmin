63a1d43558fb9a1a48c1dd708fa20c9b70e0bd5c
luorunbing <luorunbing@lvyii.com>
2017-11-04 15:56:18 +0800

    user: only filter out myself when list all admin users
    Signed-off-by: luorunbing <luorunbing@lvyii.com>

--- src/util/auth/redux.js
+++ src/util/auth/redux.js
@@ -497,6 +497,7 @@ function* sagaListAdminUsers(action) {
       mobilePhoneNumber: params.mobilePhoneNumber,
       roles: params.roles,
       status: params.status,
+      skipMyself: true,
     } = payload);
 
     // result = {
