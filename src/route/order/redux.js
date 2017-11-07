4ec25adbe7aed415279aea2b690a5313f9fbafe3
yang24201 <yang24201@126.com>
2017-11-07 09:31:31 +0800

    modify withdraw status value

--- src/route/order/redux.js
+++ src/route/order/redux.js
@@ -142,8 +142,8 @@ export const DealType = {
 }
 
 export const WITHDRAW_STATUS = {
-  APPLYING: 0,      // 提交申请
-  DONE: 1,          // 处理完成
+  APPLYING: 1,      // 提交申请
+  DONE: 2,          // 处理完成
 }
 
 export const WITHDRAW_APPLY_TYPE = {
