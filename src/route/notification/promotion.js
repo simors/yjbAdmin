9e414c98e9a681d8ed86fb3114fa105f9c0dab1b
luorunbing <luorunbing@lvyii.com>
2017-11-11 14:46:40 +0800

    user: forbid user to view unauthorized page
    Signed-off-by: luorunbing <luorunbing@lvyii.com>

--- src/route/notification/promotion.js
+++ src/route/notification/promotion.js
@@ -1,8 +1,12 @@
 import React from 'react';
 import {connect} from 'react-redux';
+import {Link} from 'react-router-dom';
 import {selector} from './redux';
 import Step1 from './promotion-step1';
 import Step2 from './promotion-step2';
+import {selector as authSelector} from '../../util/auth/';
+import {PERMISSION_CODE} from '../../util/rolePermission/';
+import Exception from '../../component/Exception/';
 
 class Promotion extends React.Component {
   constructor(props) {
@@ -10,23 +14,30 @@ class Promotion extends React.Component {
   }
 
   render() {
-    const {curStep} = this.props;
-    switch (curStep) {
-      case 1:
-        return (<Step1/>);
-      case 2:
-        return (<Step2/>);
-      default:
-        return (<Step1/>);
+    const {curStep, sendMarketingMsgVisible} = this.props;
+
+    if (sendMarketingMsgVisible) {
+      switch (curStep) {
+        case 1:
+          return (<Step1/>);
+        case 2:
+          return (<Step2/>);
+        default:
+          return (<Step1/>);
+      }
     }
+
+    return (<Exception type="403" style={{ minHeight: 500, height: '80%' }} linkElement={Link} />);
   }
 }
 
 const mapStateToProps = (appState, ownProps) => {
   const curStep = selector.selectCurStep(appState);
+  const sendMarketingMsgVisible = authSelector.selectValidPermissions(appState, [PERMISSION_CODE.PUSH_MARKETING_MSG]);
 
   return {
     curStep,
+    sendMarketingMsgVisible,
   };
 };
 
--- src/route/notification/system.js
+++ src/route/notification/system.js
@@ -1,8 +1,12 @@
 import React from 'react';
 import {connect} from 'react-redux';
+import {Link} from 'react-router-dom';
 import {selector} from './redux';
 import Step1 from './system-step1';
 import Step2 from './system-step2';
+import {selector as authSelector} from '../../util/auth/';
+import {PERMISSION_CODE} from '../../util/rolePermission/';
+import Exception from '../../component/Exception/';
 
 class System extends React.Component {
   constructor(props) {
@@ -10,23 +14,30 @@ class System extends React.Component {
   }
 
   render() {
-    const {curStep} = this.props;
-    switch (curStep) {
-      case 1:
-        return (<Step1/>);
-      case 2:
-        return (<Step2/>);
-      default:
-        return (<Step1/>);
+    const {curStep, sendSysMsgVisible} = this.props;
+
+    if (sendSysMsgVisible) {
+      switch (curStep) {
+        case 1:
+          return (<Step1/>);
+        case 2:
+          return (<Step2/>);
+        default:
+          return (<Step1/>);
+      }
     }
+
+    return (<Exception type="403" style={{ minHeight: 500, height: '80%' }} linkElement={Link} />);
   }
 }
 
 const mapStateToProps = (appState, ownProps) => {
   const curStep = selector.selectCurStep(appState);
+  const sendSysMsgVisible = authSelector.selectValidPermissions(appState, [PERMISSION_CODE.PUSH_SYSTEM_MSG]);
 
   return {
     curStep,
+    sendSysMsgVisible,
   };
 };
 
